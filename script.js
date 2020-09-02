328275c4b1a63a783b5b72ce41792e18ec45845d
Andrew Siegel <siegelal7@gmail.com>
2020-09-02 09:54:10 -0400

    added a return statement to the generatePassword function

--- script.js
+++ script.js
@@ -9,20 +9,23 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
 function generatePassword() {
   var length = prompt(
     "How long would you like your password to be? (max length 128 characters)"
   );
-  var lowerCase = prompt("Would you like to include lowercase letters?");
-  var upperCase = prompt("Would you like to include uppercase letters?");
-  var numbers = prompt("Would you like to include numbers?");
-  var chars = prompt("Would you like to include characters?");
+  var lowerCase = confirm("Would you like to include lowercase letters?");
+  var upperCase = confirm("Would you like to include uppercase letters?");
+  var numbers = confirm("Would you like to include numbers?");
+  var chars = confirm("Would you like to include characters?");
   var characters = ["!@#$%^&*()"];
   //var item = characters[Math.floor(Math.random() * characters.length)];
   //console.log(item);
   var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowerLetters = "abcdefghijklmnopqrstuvwxyz";
   var numbs = "1234567890";
+  // TODO: gen a real password
+  return "Still working..";
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
